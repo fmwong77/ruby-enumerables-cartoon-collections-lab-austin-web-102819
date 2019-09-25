25683e80c109f2db0ccd6d47ab324f8faa371983
Mei Wong <fongmeiw@gmail.com>
2019-09-25 21:20:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,12 @@
-def roll_call_dwarves(dwarves) # code an argument here
+def roll_call_dwarves(dwarves) 
   dwarves.each_with_index {|item, index|
     puts "#{index+1}. #{item}"
   }
 end
 
-def summon_captain_planet(array)# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+ array.map() do
+ end
 end
 
 def long_planeteer_calls# code an argument here
