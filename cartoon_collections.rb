c06e689f6743ac36f39333723f5c3517a05a1ebc
Mei Wong <fongmeiw@gmail.com>
2019-09-25 20:56:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves) # code an argument here
+  dwarves.each_with_index {|item, index|
+    puts item[index]
+  }
 end
 
 def summon_captain_planet# code an argument here
